85a628d9595ed1075328fca5d068fe9aa739845f
Anna-er <brungilda364@gmail.com>
2024-09-09 22:52:10 +0300

    feat: Add implementation and tests

---
+++ project/task1.py
@@ -0,0 +1,28 @@
+import cfpq_data as cd
+import networkx as nx
+from typing import Tuple
+
+
+def get_graph(name: str):
+    graph_archive = cd.download(name)
+    graph = cd.graph_from_csv(graph_archive)  # loading graph by path
+    return graph
+
+
+def graph_data(graph_name: str):
+    graph = get_graph(graph_name)
+
+    nodes = graph.number_of_nodes()
+    edges = graph.number_of_edges()
+    labels = cd.get_sorted_labels(graph)
+
+    return nodes, edges, labels
+
+
+def create_labeled_two_cycles_graph(
+    n: int, m: int, labels: Tuple[str, str], path_to_save: str
+):
+    graph = cd.labeled_two_cycles_graph(n, m, labels=labels)
+
+    graph_dot = nx.drawing.nx_pydot.to_pydot(graph)
+    graph_dot.write(path_to_save)
---
+++ tests/test_task1.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from project import task1
+
+
+class TestGraphUtils:
+    def test_gzip(self):
+        info = task1.graph_data("gzip")
+        gzip = (2687, 2293, ["d", "a"])
+        assert info == gzip
+    def test_bzip(self):
+        info = task1.graph_data("bzip")
+        bzip = (632, 556, ["d", "a"])
+        assert info == bzip
+
+    def test_writing_to_file(self):
+        file_name = "graph.dot"
+        graph_info = (12, 65, ("a", "b"))
+        task1.create_labeled_two_cycles_graph(
+            graph_info[0], graph_info[1], graph_info[2], file_name
+        )
+
+        file_path = Path(file_name)
+        assert file_path.is_file()
+
+        file_path.unlink()
